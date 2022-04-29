668ba67a7c2f2fc4b000703afd0a6cf8f76fc2af
SabahNur86 <sabahnurdagtekin@gmail.com>
2022-04-29 23:00:06 +0300

    15

--- carrentalprojectbackend/src/main/java/com/carrentalproject/CarrentalprojectApplication.java
+++ carrentalprojectbackend/src/main/java/com/carrentalproject/CarrentalprojectApplication.java
@@ -2,6 +2,11 @@ package com.carrentalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class CarrentalprojectApplication {
@@ -10,4 +15,25 @@ public class CarrentalprojectApplication {
         SpringApplication.run(CarrentalprojectApplication.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter(){
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        UrlBasedCorsConfigurationSource source =new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config =new CorsConfiguration();
+        //canlıya proje cikinca cors kontrol eden siteler var
+        //burada bu izinleri vermezsek metodlarimiz clismicaktir
+        //frontend de de kullanilamayacaktir.
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("PATCH");
+        config.addAllowedMethod("DELETE");
+        //burada izin verdigimiz metodlari yaziyoruz.
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**",config); // url bazinda izinlerimizi olusturduk
+        registrationBean.setFilter(new CorsFilter(source));
+        registrationBean.setOrder(0);
+        return registrationBean;
+    }
 }
